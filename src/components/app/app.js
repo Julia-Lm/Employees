47706fb8e3146913563d5efffca7f0e03f4b4a2a
Julia-Lm <63656237+Julia-Lm@users.noreply.github.com>
2022-03-04 08:55:06 +0200

    Add files via upload

---
+++ src/components/app/app.js
@@ -0,0 +1,176 @@
+import { Component } from 'react';
+
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                { name: "John C.", salary: 800, increase: false, rise: true, id: 1 },
+                { name: "Hanna L.", salary: 3000, increase: true, rise: false, id: 2 },
+                { name: "Marc T.", salary: 5500, increase: false, rise: false, id: 3 }
+            ],
+            term: '',
+            filter: 'all'
+        }
+        this.maxId = 4;
+    }
+
+    deleteItem = (id) => {
+        //console.log(id);
+        this.setState(({ data }) => {
+            /*1 вариант */
+            return {
+                data: data.filter(item => item.id !== id)
+                /*данные отфильтруются и останутся только те эл., уникальный идентификатор которого
+                не совпадает с тем id который нам пришел
+                и как итог возвращает новый массив*/
+            }
+
+            /*1 вариант
+            const index = data.findIndex(elem => elem.id === id);
+
+            const before = data.slice(0, index);//копируем часть массива до того эл. который нам нужен
+            const after = data.slice(index + 1);//пропускаем эл. который нашли и после него начинаем копирувать кусок масива после след элем.
+            //before и after это два массива, которые содержат половинки старого массива
+
+            const newArr = [...before, ...after];//создаем один большой масив с двух частей.
+            
+            return {
+                data: newArr//возвращаем новый скопированный массив
+            }
+            */
+        });
+    }
+
+    addItem = (name, salary) => {
+        if (name !== '' && salary !== '' && name.length > 3) {
+            const newIem = {
+                name,
+                salary,
+                increase: false,
+                rise: false,
+                id: this.maxId++
+            }
+            this.setState(({ data }) => {
+                const newArr = [...data, newIem];
+                return {
+                    data: newArr
+                }
+            });
+        }
+
+    }
+
+    /*
+    onToggleIncrease = (id) => {
+        this.setState(({ data }) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return { ...item, increase: !item.increase }
+                }
+                return item;
+            })
+        }));
+        /*2var
+        this.setState(({ data }) => {
+            const index = data.findIndex(elem => elem.id === id);
+
+            const old = data[index];
+            const newItem = { ...old, increase: !old.increase };
+            const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+            return {
+                data: newArr
+            }
+        })
+        
+    }
+
+    onToggleRice = (id) => {
+        this.setState(({ data }) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return { ...item, rise: !item.rise }
+                }
+                return item;
+            })
+        }))
+    }
+*/
+
+    //один метод обьединяющий onToggleRice и onToggleIncrease
+    //prop это или rise, или increase
+    onToggleProp = (id, prop) => {
+        this.setState(({ data }) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return { ...item, [prop]: !item[prop] }
+                }
+                return item;
+            })
+        }))
+    }
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1;
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({ term });
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({ filter });
+    }
+
+    render() {
+        const { data, term, filter } = this.state
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
+
+        return (
+            <div className="app">
+                <AppInfo employees={employees}
+                    increased={increased} />
+
+                <div className='search-panel'>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch} />
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
+                </div>
+
+                <EmployeesList
+                    data={visibleData}
+                    onDelete={this.deleteItem}
+                    onToggleProp={this.onToggleProp}
+                />
+                <EmployeesAddForm onAdd={this.addItem} />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,29 @@
+import { Component } from 'react';
+import './search-panel.css';
+
+class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+    }
+
+    onUpdateSearchLocal = (e) => {
+        const term = e.target.value;
+        this.setState({ term });
+        this.props.onUpdateSearch(term)
+    }
+
+    render() {
+        return (
+            <input type="text"
+                className="form-control search-input"
+                placeholder="Найти сотрудника"
+                value={this.state.term}
+                onChange={this.onUpdateSearchLocal} />
+        )
+    }
+}
+
+export default SearchPanel;
